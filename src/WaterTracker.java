b9d560889b937e10145c01b6eb1dde1bb52007dc
TobyR04 <76408125+TobyR04@users.noreply.github.com>
2021-12-14 14:39:14 -0500

    Added message and goal

--- src/WaterTracker.java
+++ src/WaterTracker.java
@@ -1,32 +1,41 @@
 package health;
 
 public class WaterTracker {
-	
-	private final int goal = 64; 
+
+	private int goal;
 	private double currentIntake;
-	
-	public WaterTracker() {
+
+	public WaterTracker(int goal) {
+		this.goal = goal;
 		currentIntake = 0;
 	}
-	
+
 	public void addWater(double waterIntake) {
-		
+
 		if (waterIntake > 0) {
 			currentIntake += waterIntake;
 		}
 	}
-	
+
 	public double getCurrentIntake() {
-		
+
 		return currentIntake;
 	}
-	
+
 	public boolean reachedGoal() {
-		
+
 		return currentIntake >= goal;
 	}
-	
+
+	public String getGoalMessage() {
+		String message = "You haven't drank enough today! Drink more! You don't want to dehydrate!";
+		if (reachedGoal()) {
+			message = "Good job! You've drinken enough today";
+		} 
+		return message;
+	}
+
 	public String toString() {
-		return "Your water intake is " + currentIntake + " oz.";
+		return "Your water intake is " + currentIntake + " oz. \n"   + getGoalMessage();
 	}
 }
